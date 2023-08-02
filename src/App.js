6f8f00cde4f183096679c1c1e4b16b53aa8d283f
klanti117 <klantyislam@yahoo.com>
2023-08-02 11:39:32 -0400

    getting currency

--- src/App.js
+++ src/App.js
@@ -11,7 +11,18 @@ function App() {
     );
     const data = await res.json();
     setCountries(data);
-    console.log(typeof data)
+    
+    const { language } = data[0]
+    // console.log(Object.values(language))
+
+    const { currencies } = data[0]
+    console.log( (Object.values(currencies)[0]).name )
+    // console.log(Object.values(currencies[0]).name)
+
+    // const apiDynamicKey = 'PEN'
+    // const { currencies:{ PEN: { name } } } = data[0]
+    // const { currencies:{ [apiDynamicKey]: { name } } } = data[0]
+    // console.log(name)
   };
 
   return (
@@ -19,7 +30,9 @@ function App() {
       <h1>30 Days Of React: Day6</h1>
       <button onClick={getData}>Select Your Destination</button>
         <div>
-        {countries.map(({name, capital,population, flags:{png, alt} }) => (
+        {countries.map(({name, capital, population, flags: {png, alt}, 
+          language, currencies
+        }) => (
           <div key={name}>
             <Card 
               name={name.common} 
@@ -27,8 +40,10 @@ function App() {
               population={population}  
               flagSrc={png}
               flagAlt={alt}
+              // language={getLang('key',language)}
+              currencies={getCurrency(currencies)}
               />
-              <h1>end of </h1>
+              <h1>---------------------------------------</h1>
           </div>
           ))}
         </div> 
@@ -37,3 +52,12 @@ function App() {
 }
 
 export default App;
+
+const getLang = (str, language) =>{
+  const { [str]: returnValue } = language;   
+    return returnValue;
+}
+
+const getCurrency = (currencies) =>{
+  return (Object.values(currencies)[0]).name 
+}
